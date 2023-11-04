3f5bd3bd6ffcbff0a4036424bc70eceffbef8962
z8k3 <144612805+z8k3@users.noreply.github.com>
2023-11-03 21:01:30 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
 
+int main() {
+  std::cout << "Weight" << std::endl;
+  double weight;
+  std::cin >> weight;
+  std::cout << "Gravity" << std::endl;
+  double width;
+  std::cin >> width;
+  std::cout << "Height" << std::endl;
+  double height;
+  std::cin >> height;
+  double PEg = weight * width * height;
+  std::cout << PEg << std::endl;
+}
